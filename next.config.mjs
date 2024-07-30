c122d7df38a00af42c8f0736dc8d2d00b5aa3af5
Harshid Khunt <harshid.khunt@logicwind.com>
2024-07-30 14:45:52 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -34,28 +34,6 @@ const nextConfig = {
     });
     return headers;
   },
-  async rewrites() {
-    return [
-      {
-        source: '/__/auth/:path*',
-        destination: `https://my-project-2-4e46d.firebaseapp.com/__/auth/:path*`,
-      },
-    ];
-  },
-  env: {
-    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
-    NEXT_SERVER_URL: process.env.NEXT_SERVER_URL,
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
-      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
-      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
-      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
-      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  },
 };
 
 export default nextConfig;
