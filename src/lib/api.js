76319d1c1969e7de5bd1cc273b99663dee8be561
Harshid Khunt <harshid.khunt@logicwind.com>
2024-07-30 16:36:42 +0530

    fixes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.BE_HOST}${process.env.GOOGLE_LOGIN_ENDPOINT}`,
+  baseURL: `${process.env.NEXT_PUBLIC_BE_HOST}${process.env.NEXT_PUBLIC_GOOGLE_LOGIN_ENDPOINT}`,
   headers: {
     'Content-Type': 'application/json',
   },
