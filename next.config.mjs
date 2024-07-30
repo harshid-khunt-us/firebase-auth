4f6dd433e680db17a47b09d0da0ac45a85f64be0
Harshid Khunt <harshid.khunt@logicwind.com>
2024-07-30 13:02:58 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -38,7 +38,7 @@ const nextConfig = {
     return [
       {
         source: '/__/auth/:path*',
-        destination: `https://${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}/__/auth/:path*`,
+        destination: `https://my-project-2-4e46d.firebaseapp.com/__/auth/:path*`,
       },
     ];
   },
--- src/app/page.js
+++ src/app/page.js
@@ -47,7 +47,7 @@ export default function SignIn() {
       ) : (
         <>
           <button onClick={handleGoogleLogin}>google</button>
-          <div>new1</div>
+          <div>new2</div>
         </>
       )}
     </div>
