ab776e6556beb4aa69527e671a86f32e9817d68f
Harshid Khunt <harshid.khunt@logicwind.com>
2024-07-30 14:05:48 +0530

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -47,7 +47,7 @@ export default function SignIn() {
       ) : (
         <>
           <button onClick={handleGoogleLogin}>google</button>
-          <div>new2</div>
+          <div>new3</div>
         </>
       )}
     </div>
